af983ab41c3dfa89ed580a4a15a35bdf9bf73f05
Alejo-Chavez <aledchav@example.com>
2026-05-10 21:40:00 -0300

    FIX: arreglando las columnas del div y detalles menores

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -10,12 +10,24 @@ import { RouterModule } from '@angular/router';
 export class Home {
   games = [{
     title: 'Juego 1',
-    description: 'Descripción del juego 1',
+    description: 'Tendrás la suerte necesaria para adivinar si tu siguiente será mayor o menor?',
     route: '/games/ahorcado',
     icon: 'fa-gamepad'
   },
   {
     title: 'Juego 2',
+    description: 'Tendrás la suerte necesaria para adivinar si tu siguiente será mayor o menor?',
+    route: '/mayor-o-menor',
+    icon: 'fa-chess'
+},
+{
+    title: 'Juego 3',
+    description: 'El mitico juego de ahorcado que todos alguna vez jugamos',
+    route: '/mayor-o-menor',
+    icon: 'fa-chess'
+},
+{
+    title: 'Juego 4',
     description: 'Descripción del juego 2',
     route: '/mayor-o-menor',
     icon: 'fa-chess'
